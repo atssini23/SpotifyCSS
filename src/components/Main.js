6154072306ed8ac96e8660b9a306355aca634f6d
Atssini Cabrera <atssini23@yahoo.com>
2021-01-27 14:55:53 -0800

    see all update

--- src/components/Main.js
+++ src/components/Main.js
@@ -22,21 +22,62 @@ const Main = () => {
         </div>
         <div className="cardsWrap">
           <h2>Focus</h2>
+          <span className="seeAll">See All</span>
           <p className="subText">Music to help you concentrate.</p>
-          <div className="card">
-            <div className="cardImage">
-              <img
-                src="https://images.unsplash.com/photo-1434907714621-289291ffea37?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1650&q=80"
-                alt="Pic 1"
-              />
+
+          <div className="cardsWrapInner">
+            <div className="card">
+              <div className="cardImage">
+                <img
+                  src="https://images.unsplash.com/photo-1434907714621-289291ffea37?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1650&q=80"
+                  alt="Pic 1"
+                />
+              </div>
+              <div className="cardContent">
+                <h3>Music for concentration</h3>
+                <span>Minimalism, electronica and modern...</span>
+              </div>
+              <span className="playIcon">></span>
             </div>
-            <div className="cardContent">
-              <h3>Music for concentrate</h3>
-              <span>
-                Minimalism, electronica and modern classical to concentrate to.
-              </span>
+            <div className="card">
+              <div className="cardImage">
+                <img
+                  src="https://images.unsplash.com/photo-1434907714621-289291ffea37?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1650&q=80"
+                  alt="Pic 1"
+                />
+              </div>
+              <div className="cardContent">
+                <h3>Music for concentration</h3>
+                <span>Minimalism, electronica and modern...</span>
+              </div>
+              <span className="playIcon">></span>
             </div>
-            <span className="playIcon">></span>
+            <div className="card">
+              <div className="cardImage">
+                <img
+                  src="https://images.unsplash.com/photo-1434907714621-289291ffea37?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1650&q=80"
+                  alt="Pic 1"
+                />
+              </div>
+              <div className="cardContent">
+                <h3>Music for concentration</h3>
+                <span>Minimalism, electronica and modern...</span>
+              </div>
+              <span className="playIcon">></span>
+            </div>
+            <div className="card">
+              <div className="cardImage">
+                <img
+                  src="https://images.unsplash.com/photo-1434907714621-289291ffea37?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1650&q=80"
+                  alt="Pic 1"
+                />
+              </div>
+              <div className="cardContent">
+                <h3>Music for concentration</h3>
+                <span>Minimalism, electronica and modern...</span>
+              </div>
+              <span className="playIcon">></span>
+            </div>
           </div>
         </div>
       </div>
