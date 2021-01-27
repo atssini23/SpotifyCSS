cfd0713c695fb49d13ea2586233105f2fda8bcdc
Atssini Cabrera <atssini23@yahoo.com>
2021-01-27 13:28:11 -0800

    main playlists

--- src/components/Main.js
+++ src/components/Main.js
@@ -5,8 +5,8 @@ const Main = () => {
     <div className="main">
       <div className="upperNav">dummy text</div>
       <div className="mainContent">
-        <h1>Uniquely yours</h1>
         <div className="cardsWrap">
+          <h2>Uniquely yours</h2>
           <div className="card">
             <div className="cardImage">
               <img
@@ -17,6 +17,26 @@ const Main = () => {
             <div className="cardContent">
               <h3>Liked Songs</h3>
             </div>
+            <span className="playIcon">></span>
+          </div>
+        </div>
+        <div className="cardsWrap">
+          <h2>Focus</h2>
+          <p className="subText">Music to help you concentrate.</p>
+          <div className="card">
+            <div className="cardImage">
+              <img
+                src="https://images.unsplash.com/photo-1434907714621-289291ffea37?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1650&q=80"
+                alt="Pic 1"
+              />
+            </div>
+            <div className="cardContent">
+              <h3>Music for concentrate</h3>
+              <span>
+                Minimalism, electronica and modern classical to concentrate to.
+              </span>
+            </div>
+            <span className="playIcon">></span>
           </div>
         </div>
       </div>
